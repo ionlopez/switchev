cc05d1133029ee6d3e04218d63533aa8bca39a0a
Ion Lopez <jonlopez@gmail.com>
2021-08-05 16:29:23 +0100

    First commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React from 'react';
+import type { AppProps } from 'next/app';
+
+import { Page } from 'components/Page';
+
+import '../styles/globals.css';
+import 'tailwindcss/tailwind.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Page>
+      <Component {...pageProps} />
+    </Page>
+  );
 }
-export default MyApp
+export default MyApp;
